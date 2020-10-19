7560b5e954079f3bf8e8a200692368449966f90d
MPiotrowska <monikapiotrowska27@gmail.com>
2020-10-19 14:06:53 +0200

    move blog posts under blog folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,8 +12,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `src`,
-        path: `${__dirname}/blog`,
+        name: `blog`,
+        path: `${__dirname}/src/blog`,
       },
     },
     `gatsby-transformer-remark`,
